d24b719ef652c6521d0dd523ecca5d4fcb935df7
cristiano <cac.lopez@yavirac.edu.ec>
2021-04-23 19:55:11 -0500

    logo y perfil de unusrio modulo aumentadoi

---
+++ .history/backend/views/modules/inicio_20210407123339.php
@@ -0,0 +1,102 @@
+<?php
+
+session_start();
+
+if(!$_SESSION["validar"]){
+
+	header("location:ingreso");
+
+	exit();
+
+}
+
+include "views/modules/botonera.php";
+include "views/modules/cabezote.php";
+
+?>
+
+<!--=====================================
+INICIO       
+======================================-->
+
+<div id="inicio" class="col-lg-10 col-md-10 col-sm-9 col-xs-12">
+<!-- <img src="views/images/logotipo.jpg" class="img-responsive" alt="Image"> -->
+
+	<div class="jumbotron" style="background-image: url('https://static.vecteezy.com/system/resources/previews/001/226/022/non_2x/background-blue-and-white-squares-vector.jpg');background-repeat: no-repeat;background-size: cover;font-family: Ginebra;" class="img-responsive">
+
+	    <h1>Bienvenidos</h1>
+	    <p>Al panel de control de Wayrabamba.</p>
+	</div>
+
+		<hr>
+<!-- 	
+	<ul>
+
+		<li class="botonesInicio">
+		
+			<a href="slide">
+			<div style="background:#4CF53A">
+			<span class="fa fa-toggle-right"></span>
+			<p>Slide</p>
+			</div>
+			</a>
+
+		</li>
+
+		<li class="botonesInicio">
+		
+			<a href="articulos">
+			<div style="background:#F640DA">
+			<span class="fa fa-file-text-o"></span>
+			<p>Artículos</p>
+			</div>
+			</a>
+
+		</li>
+
+		<li class="botonesInicio">
+		
+			<a href="galeria">
+			<div style="background:#04E6DE">
+			<span class="fa fa-image"></span>
+			<p>Imágenes</p>
+			</div>
+			</a>
+
+		</li>
+
+		<li class="botonesInicio">
+		
+			<a href="videos">
+			<div style="background:#1434AD"> 
+			<span class="fa fa-film"></span>
+			<p>Videos</p>
+			</div>
+			</a>
+
+		</li>
+
+		<?php 
+
+	  	// if($_SESSION["rol"] == 0){
+
+		// 	echo '<li class="botonesInicio">
+			
+		// 		<a href="suscriptores">
+		// 		<div style="background:#ED3E3E">
+		// 		<span class="fa fa-users"></span>
+		// 		<p>Suscriptores</p>
+		// 		</div>
+		// 		</a>
+
+		// 	</li>';
+
+		// }
+		?>
+
+	</ul> -->
+
+</div>
+
+
+<!--====  Fin de INICIO  ====-->
