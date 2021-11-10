784b538ffd9162ddcd89b9db14f2f7dc82e63794
cristiano <cac.lopez@yavirac.edu.ec>
2021-11-10 18:33:40 -0500

    sidebar corregido, para mostrar en telefono, cambio de icono de chatbot en front

---
+++ .history/views/js/app_20211109120757.js
@@ -0,0 +1,9 @@
+const chatButton = document.querySelector('.chatbox__button');
+const chatContent = document.querySelector('.chatbox__support');
+const icons = {
+    isClicked: '<img src="./views/images/chatbot5.gif"/>',
+    isNotClicked: '<img src="./views/images/chatbot6.gif"/>'
+}
+const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
+chatbox.display();
+chatbox.toggleIcon(false, chatButton);
